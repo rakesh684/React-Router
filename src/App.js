7bb045d440c960c7d3633e48200bb039b4181fa0
Rakesh kumar <dr.rakesh684@gmail.com>
2022-07-25 18:28:20 +0530

    using routeter,routes,route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react"
 import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Header from "./component/Header"
+import ServiceDetail from "./pages/services/ServiceDetail";
+// import ServicesData from "./pages/services/ServicesData";
+import ServicesList from "./pages/services/ServicesList" 
 
-function App() {
-  return (
-    <h1>Rakesh</h1>
-  )
+function App() {    
+    return (
+   
+        <div>
+            <Header />
+           
+            <Routes>
+                <Route  path="/" element={<Home />} />
+                <Route exact path="/services" element={<ServicesList />}/>
+                <Route path="/services/:serviceId" element={<ServiceDetail />} />
+            </Routes>
+            
+            
+        </div>
+       
+    )
 }
 
-export default App;
+export default App
+
+
